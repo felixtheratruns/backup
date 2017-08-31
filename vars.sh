8dbaad6369701cc5d04f73568e90db50aacff6f9
Joel <joel2.718@gmail.com>
2017-08-31 15:54:16 -0400

    got rid of destination folders

--- vars.sh
+++ vars.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
+#this gets the directory of this script for this example
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
 #settings for first part of program
 #this is what is used to remove characters that make rsync fail from the file and folder paths
 sedvar='s/[\:\|\?\"]//g'
 #settings for second part
 #set origin
-origin="/data/backup/backup_src/origin/"
+origin="$DIR/origin/"
 
-#folder to copy them to (will append folder name to origin" e.g. "other_backup" will mean /data/backup/backup_src/other_backup
+#folder to copy them to (will append folder name to origin" e.g. "other_backup" will mean $DIR/other_backup
 other_folder="other_backup/"
 
 # things to copy to origin before backup
-declare -A other_copying=( ["/data/backup/backup_src/other_origin/"]="${origin}${other_folder}another_folder/" )
+declare -A other_copying=( ["$DIR/other_origin/"]="${origin}${other_folder}another_folder/" )
 
 #this cleans out 'weird' characters that sometimes makes rsync fail
 #if you select "Just remove characters" this will happen immediately
@@ -21,7 +30,7 @@ clean_origin=true
 clean_other_copying=false
 
 #set destinations
-basedest="/data/backup/backup_src/"
+basedest="$DIR/"
 dests=( "${basedest}testdest1/" "${basedest}testdest2/" "${basedest}testdest3/"  "${basedest}testdest4/" )
 rsync_opt1="-auv"
 rsync_opt2="--delete-after"
