82ac6d39347d15282843b0ef2fbdc7ca44020721
Joel <joel2.718@gmail.com>
2017-08-31 16:59:50 -0400

    fixed readme and renamed file

--- create_and_clear_destination_dirs.sh
+++ create_and_clear_destination_dirs.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 #clears test directories I created
 . vars.sh
+
+mkdir -p $DIR/testdest1
+mkdir -p $DIR/testdest2
+mkdir -p $DIR/testdest3
+mkdir -p $DIR/testdest4
+
 for dest in ${dests[@]}; do
   find "$dest" -mindepth 1 -type f -name '*' -exec bash -c '
       for file do
